fae124fbb22abb4c9ba721e67c09516771d9b29f
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-10-14 16:07:33 +0800

    updated route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,13 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                // ->prefix('api')
+            // Route::middleware('api')
+            //     ->prefix('api')
+            //     ->group(base_path('routes/api.php'));
+
+            Route::prefix('v1')
+                ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
