efcde6e3357fe96deecd654a94ae8c85f5613fc5
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-10-15 10:34:00 +0800

    fix

--- app/Http/Controllers/Platform/LoginTaxpayer.php
+++ app/Http/Controllers/Platform/LoginTaxpayer.php
@@ -21,28 +21,16 @@ class LoginTaxpayer extends Controller
             'scope' => 'InvoicingAPI',
         ]);
 
-        // if ($response->successful()) {
-        //     $headers = $response->headers();
-
-        //     // Filter and keep only the headers you want to pass along
-        //     $relevantHeaders = array_filter($headers, function($key) {
-        //         return in_array(strtolower($key), [
-        //             'X-Rate-Limit-Remaining',
-        //             'x-rate-limit-reset',
-        //             'x-rate-limit-limit'
-        //         ]);
-        //     }, ARRAY_FILTER_USE_KEY);
-
-        //     return response()->json($response->json())
-        //         ->withHeaders($relevantHeaders);
-        // }
-
         if ($response->successful()) {
             $headers = $response->headers();
 
-            // Pass through all X-Rate-Limit headers
-            $relevantHeaders = array_filter($headers, function ($key) {
-                return strpos(strtolower($key), 'x-rate-limit') === 0;
+            // Filter and keep only the headers you want to pass along
+            $relevantHeaders = array_filter($headers, function($key) {
+                return in_array(strtolower($key), [
+                    'X-Rate-Limit-Remaining',
+                    'X-Rate-Limit-Reset',
+                    'X-Rate-Limit-Limit'
+                ]);
             }, ARRAY_FILTER_USE_KEY);
 
             return response()->json($response->json())
